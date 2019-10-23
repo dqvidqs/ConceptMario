6124e7715ee15d7bf7ac25a8b2f359786bcad11c
dqvidqs <dqvidqs@gmail.com>
2019-10-23 14:07:40 +0300

    add folder

---
+++ ConceptMario/Assets/MapBuilder/Objects/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Shapes;
+
+namespace ConceptMario
+{
+    abstract class Block
+    {
+        // YY --- YX
+        // |       |
+        // |       |
+        // XY --- XX
+        protected int[] XX = new int[2];
+        protected int[] XY = new int[2];
+        protected int[] YX = new int[2];
+        protected int[] YY = new int[2];
+        protected abstract void Draw();
+        protected char Indicator;
+        public abstract Polygon Get();
+        public abstract char GetIndicator();
+        public abstract int GetX();
+        public abstract int GetY();
+    }
+}
---
+++ ConceptMario/Assets/MapBuilder/Objects/Wall.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Drawing;
+using System.Windows;
+
+namespace ConceptMario
+{
+    class Wall : Block
+    {
+        //Wall - turintis savo grafini blocko ivaizdi
+        private Polygon Terrain = null;
+        private int Size = MetaData.Size;
+        public Wall(int X, int Y, char Indicator)
+        {
+            this.Indicator = Indicator;
+            XY[0] = X;
+            XY[1] = Y;
+            XX[0] = X + Size;
+            XX[1] = Y;
+            YY[0] = X;
+            YY[1] = Y + Size;
+            YX[0] = X + Size;
+            YX[1] = Y + Size;
+            Draw();
+        }
+        protected override void Draw()
+        {
+            Terrain = new Polygon();
+            Terrain.Stroke = Brushes.Black;
+            Terrain.Fill = Brushes.Red;
+            Terrain.Points = new PointCollection() { new Point(0, 0), new Point(Size, 0), new Point(Size, Size), new Point(0, Size) };
+        }
+        public override Polygon Get()
+        {
+            return Terrain;
+        }
+        public bool CheckUp(int X, int Y)
+        {
+            if (Y + Size <= XY[1])
+                return true;
+            else
+                return false;
+        }
+        public bool CheckDown(int X, int Y)
+        {
+            if (Y <= YY[1])
+                return true;
+            else
+                return false;
+        }
+        public bool CheckRight(int X, int Y)
+        {
+            if (X + Size <= XY[0])
+                return true;
+            else
+                return false;
+        }
+        public bool CheckLeft(int X, int Y)
+        {
+            if (X <= XX[0])
+                return true;
+            else
+                return false;
+        }
+        public override int GetX() { return XY[0]; }
+        public override int GetY() { return XY[1]; }
+        public override char GetIndicator()
+        {
+            return Indicator;
+        }
+    }
+}
