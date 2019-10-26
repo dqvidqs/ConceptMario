b75819909a63723d6f4e2cf109f0aa80722bf2f7
dqvidqs <dqvidqs@gmail.com>
2019-10-26 14:59:37 +0300

    Add Gun Mechanics

--- Objects/Models/User.cs
+++ Objects/Models/User.cs
@@ -6,6 +6,7 @@ namespace Objects.Models
 		public int id { get; set; }
 		public string username { get; set; }
 		public string password { get; set; }
-        public int gold { get; set; }
+		public int gold { get; set; }
+		public byte status { get; set; }
 	}
 }
