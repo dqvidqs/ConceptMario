6faf5682276023d21711a2b54b58bf6c06801049
dqvidqs <dqvidqs@gmail.com>
2019-11-06 13:33:18 +0200

    small updates

---
+++ ConceptMario/MetaData.cs
@@ -0,0 +1,18 @@
+﻿namespace ConceptMario
+{
+    static class MetaData
+    {
+        //GLOBAL VALUES
+        private static int _widthpx = 900;//pixel
+        private static int _heightpx = 600;//pixel
+        private static int _size = 25;//pixel
+        private static double _fps = 0.0166;//60Hz
+        //take GLOBAL values
+        public static int Width { get { return _widthpx / _size; } }//plotis langeliu skaičius
+        public static int Height { get { return _heightpx / _size; } }//aukštis langelių skaičius
+        public static int WidthPx { get { return _widthpx; } }
+        public static int HeightPx { get { return _heightpx; } }
+        public static int Size { get { return _size; } }
+        public static double FPS { get { return _fps; } }		
+	}
+}
