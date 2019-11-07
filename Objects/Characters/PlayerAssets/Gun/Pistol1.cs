e8939a60aca25ab38c1ac9571e510bb4fc2123e0
Dovydas Jurkonis <dovydas.jurkonis@gmail.com>
2019-11-07 21:12:46 +0200

    fixed

--- Objects/Characters/PlayerAssets/Gun/Pistol1.cs
+++ Objects/Characters/PlayerAssets/Gun/Pistol1.cs
@@ -8,7 +8,7 @@ namespace ConceptMario.Assets.Characters.PlayerAssets
 {
     public class Pistol1 : Gun
     {
-        public Pistol1() : base(FireRate: 60, Ammo: 7, "Useless pistol")
+        public Pistol1() : base(FireRate: 60, Ammo: 7)//, "Useless pistol")
         {
         }
     }
