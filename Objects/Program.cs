d563c8f5d7f881621cfc9b6955285ef0d2a2d440
dqvidqs <dqvidqs@gmail.com>
2019-11-14 15:47:13 +0200

    n

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,8 @@ using System.Threading.Tasks;
 
 namespace Objects
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// The code provided will print ‘Hello World’ to the console.
-			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-			Console.WriteLine("Hello World!");
-			Console.ReadKey();
-
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
-		}
-	}
+    class Program
+    {
+        public static void Main(string[] args) { }
+    }
 }
